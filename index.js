ec061d0b20cba1375c9b2ea2507e9aef010cdeb4
Imran Iqbal <imran1337@gmail.com>
2019-08-12 12:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// here we define the function take a number with 2 parameters
-function takeANumber(people, name) {
-// here we are adding the persons name to the people Array
-people.push(name)
-// here we return the persons name along with the length of the queue
-  return `Welcome, ${name}. You are number ${people.length} in line.`
-}
+// // here we define the function take a number with 2 parameters
+// function takeANumber(people, name) {
+// // here we are adding the persons name to the people Array
+// people.push(name)
+// // here we return the persons name along with the length of the queue
+//   return `Welcome, ${name}. You are number ${people.length} in line.`
+// }
 
 // creates new function nowServing with 1 parameter
 function nowServing(people) {
